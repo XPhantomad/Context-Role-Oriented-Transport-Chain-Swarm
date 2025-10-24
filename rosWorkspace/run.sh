ce79ee89d792d8466d2fffddd947210dc6f9518d
Adrian Scholze <adrian.scholze@mailbox.tu-dresden.de>
2025-10-24 10:20:26 +0200

    add docker file for simplified execution

---
+++ rosWorkspace/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run -it --net=host --ipc=host --privileged \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="${XAUTHORITY}:/root/.Xauthority" \
+    argos3-ros2-fchain \
+    bash -c "cd ros_ws && source install/setup.bash && argos3 -c bridge_example.argos &; cd Context-Role-Oriented-Transport-Chain-Swarm && python3 startup/automatedStartup.py"
