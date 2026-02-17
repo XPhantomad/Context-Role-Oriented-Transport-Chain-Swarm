0a0341b45fc1d3c91eda9ca6cbe45370fa19750c
Adrian Scholze <adrian.scholze@mailbox.tu-dresden.de>
2026-02-17 15:38:26 +0100

    edit docker run script to use dockerhub image

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker run -it --net=host --ipc=host --privileged \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="${XAUTHORITY}:/root/.Xauthority" \
-    argos3-ros2-tchain \
+    xphantomad/argos3-ros2-tchain:latest \
     bash -c "cd ros_ws && source install/setup.bash && argos3 -c transportChainSwarm.argos & cd .. && export PATH=/julia-1.10.10/bin:$PATH && cd Context-Role-Oriented-Transport-Chain-Swarm && source /ros_ws/install/setup.bash && source startup/bin/activate && python3 startup/automatedStartup.py"
